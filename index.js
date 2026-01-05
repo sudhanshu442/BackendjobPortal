863cee209a6513b650693153f872b6f71ac21a6d
Sudhanshu <sudhanshuyadav1665@gmail.com>
2026-01-06 01:20:34 +0530

    index

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions={
-    origin:"http://localhost:5173",
+    origin:["http://localhost:5173","https://job-portal-web1665.netlify.app"],
     credentials:true,
 };
 app.use(cors(corsOptions));
